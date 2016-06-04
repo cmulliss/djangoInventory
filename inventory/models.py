b2be171bfcf74152db868df7f7229f715f4c071a
Cherry Mulliss <cmulliss@gmail.com>
2016-06-04 16:52:11 +0100

    first commit

---
+++ inventory/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    amount = models.IntegerField()
+    
+
+    
